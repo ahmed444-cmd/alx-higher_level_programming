39e229dabfe272cb517544a9870e31059fca8154
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-03-23 18:47:15 -0700

    plindrome

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,7 @@
+#include <stddef.h>
 #ifndef LISTS_H
 #define LISTS_H
-#include <stddef.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -21,4 +22,5 @@ void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
 
-#endif /* LISTS_H */
+#endif
+
