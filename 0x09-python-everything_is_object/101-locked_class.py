1b75231df9a8835c8c3000f7a8d33fed5aeed56d
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-04-09 15:57:31 -0700

    commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""definition of LockedClass."""
+
 
 class LockedClass:
-    
+    """
+    Prevent the user from new LockedClass attributes
+    for anything except attributes called 'first_name'.
+    """
+
     __slots__ = ["first_name"]
