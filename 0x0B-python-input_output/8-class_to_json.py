cd811c0e5dd91d15dba62ec49fa2cf3c0624cea0
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-04-17 15:30:22 -0700

    in/out

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Def class_to_json()
+"""
+
+
+def class_to_json(obj):
+    """
+        returns the dictionary descript with simple data struct.
+    """
+    return obj.__dict__
