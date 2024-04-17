6ffcd0f5cc68162710109225578ce494d91be232
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-04-17 12:05:33 -0700

    inherit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,17 +6,3 @@
 def lookup(obj):
     """Returns a list of an object's attribs."""
     return (dir(obj))
-
-0x0A-python-inheritance/1-my_list.py
-
-#!/usr/bin/python3
-
-"""Definition of an inherited lst clss MyList."""
-
-
-class MyList(list):
-    """Sorted printing for the built-in lit clss."""
-
-    def print_sorted(self):
-        """Prints a lst in ascending order."""
-        print(sorted(self))
