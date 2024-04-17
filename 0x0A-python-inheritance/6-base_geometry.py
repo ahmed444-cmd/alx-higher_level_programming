467c4e0d46895b40d8d5f91aea3ac802ff8947e9
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-04-17 13:21:12 -0700

    inherit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Definition of  a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implement."""
+        raise Exception("area() is not implemented")
