45c1de9809156d47480f13f36b017153fc5963eb
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-04-17 13:01:16 -0700

    inherit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,7 +10,7 @@ def inherits_from(obj, a_class):
         a_class (type): The class to match.
     Returns:
         If obj is an inherited instance of a_class - True.
-        Else - False.
+        Otherwise - False.
     """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
