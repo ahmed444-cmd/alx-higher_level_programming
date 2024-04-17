cca270a936b4fb9480f0662f9ed330b12ee0ee56
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-04-17 15:07:47 -0700

    in/out

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""
+    Definition of function from_json_string()
+"""
 
-"""Definition of a JSON-to-object function."""
+
 import json
 
 
 def from_json_string(my_str):
-    """Returns the Python object of a JSON string."""
-    return json.loads(my_str)
-
+    """
+        returns an object by a JSON string.
+    """
+    return (json.loads(my_str))
