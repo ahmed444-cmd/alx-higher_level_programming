82c0408c266609a1c0f68cbc62b02df92fcab98e
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-05-22 13:52:25 -0700

    mandatory

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,3 +1,5 @@
+-- lists all shows in the database hbtn_0d_tvshows
+-- lists all rows of tables in the database
 SELECT title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
 WHERE tv_show_genres.show_id IS NULL
