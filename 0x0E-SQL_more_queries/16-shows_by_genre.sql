8f614d93f1a91724f3ec677cdec1a2527d43491e
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-05-22 14:12:44 -0700

    mandatory

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,3 +1,5 @@
+-- lists all Comedy shows in the database hbtn_0d_tvshows
+-- lists all rows of the database ofa column value
 SELECT title, tv_genres.name FROM tv_shows
 LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
