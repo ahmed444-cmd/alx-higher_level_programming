7d51f86dadd6ea9f3a61fc4d425f349c58615f49
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-05-22 14:08:45 -0700

    mandatory

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,5 @@
+-- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+-- usethe databse to lists all rows in a table
 SELECT name FROM tv_genres
 JOIN tv_show_genres ON id=tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
