110211c247175f1d91afb02bb155cf0e9cb09beb
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-05-22 13:37:20 -0700

    mandatory

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,9 +1,7 @@
---creates database hbttn0dusa in mysql
---creates data
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- use a database
 USE hbtn_0d_usa;
---creates table hbtn0usastates in database
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-       name VARCHAR(256) NOT NULL);
+-- creates a table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
