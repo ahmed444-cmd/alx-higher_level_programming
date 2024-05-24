635fb0a4edbebe429fb973429c93dab4badf98aa
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-05-24 09:09:28 -0700

    mandatory

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all rows of the table second_table
+-- From the datab hbtn_0c_0
+-- Results display score and name
+-- Ordered by score
+SELECT score, name FROM second_table ORDER BY score DESC;
