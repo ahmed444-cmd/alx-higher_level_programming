29abf2130887ad96dbe3548867263023d175c3ea
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-05-24 09:12:24 -0700

    mandatory

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- Lists all rows of the tbl second_table
+-- From the data hbtn_0c_0
+-- Display score and name
+-- Ordered by score
+-- With score >= 10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
