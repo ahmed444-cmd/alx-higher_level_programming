d4032ad408005563b8cc4a9cfd6f72df766b721c
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-07-05 10:49:18 -0700

    Mandatory

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header 
-variable from a request to a specified URL.
+"""A script that:
+accepts a URL as input,
+sends an HTTP request to the specified URL,
+outputs the response body.
 """
 import sys
 import requests
@@ -10,4 +12,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     r = requests.get(url)
-    print(r.headers.get("X-Request-Id"))
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
