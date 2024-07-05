11220d823a8aea740631eb6730631b15e5bc3690
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-07-05 11:01:32 -0700

    Mandatory

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """A script that
-- takes in a URL
-- sends a request to the URL
-- displays the body of the response.
-"""
-import sys
-import requests
+takes a URL as input
+sends an HTTP request to the given URL
+displays the X-Request-Id header from the response """
+from requests import get
+from sys import argv
+
+
+def get_alx_intranet(url):
+
+    if url:
+        try:
+            return get(url).headers.get('X-Request-Id')
+        except Exception as e:
+            return e
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
-    else:
-        print(r.text)
+    print(get_alx_intranet(argv[1]))
