c1ad7e9d86391a337ae25c772bdc23af90d0cd89
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-07-31 07:49:54 -0700

    Mandatory

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-
-// Importing the necessary modules.
-const fs = require('fs');
 const request = require('request');
+const fs = require('fs');
 
-// Utilizing command line arguments to retrieve the source URL and specify the destination file path.
-const sourceURL = process.argv[2];
-const destinationFilePath = process.argv[3];
-
-// Sending a request to the source URL and directing the response to a writable stream.
-request(sourceURL).pipe(fs.createWriteStream(destinationFilePath));
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    fs.writeFileSync(process.argv[3], body);
+  }
+});
