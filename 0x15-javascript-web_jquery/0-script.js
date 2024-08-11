c6c4ac20c958a75e23b551205efed0835ff892ff
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-08-11 09:39:26 -0700

    Mandatory

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,2 @@
-#!/usr/bin/node
 const header = document.querySelector('HEADER');
 header.style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+let url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
+$.get(url, function (data) {
+  let films = data.results;
+  for (let film of films) {
+    $('ul#list_movies').append(`<li>${film.title}</li>`);
+  }
+});
